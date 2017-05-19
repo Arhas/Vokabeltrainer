8c47eb5e137ac17929d515597991181fd46f9a5d
Willy <15kha@cloak.mafiasi.de>
2017-05-19 15:21:24 +0200

    Kommentare Hinzugefügt

--- src/fachwert/Vokabel.java
+++ src/fachwert/Vokabel.java
@@ -1,21 +1,34 @@
 package fachwert;
-
+/*
+ * Eine Vokabel besteht aus einem Fachwort und der Zugehörigen Definition. Einmal erstellt, soll das Objekt unverändert bleiben.
+ */
 public class Vokabel
 {
-public String _fachwort;
-public String _definition;
+public final String FACHWORT;
+public final String DEFINITION;
 
+    
     public Vokabel(String fachwort, String definition)
     {
-        _fachwort = fachwort;
-        _definition = definition;
+        FACHWORT = fachwort;
+        DEFINITION = definition;
     }
+    
+    /**
+     * Gibt die zugehörige Definition zurück
+     * @return DEFINITION
+     */
     public String getDefinition()
     {
-        return _definition;
+        return DEFINITION;
     }
+    
+    /**
+     * Gibt das zugehörige Fachwort zurück
+     * @return FACHWORT
+     */
     public String getFachwort()
     {
-        return _fachwort;
+        return FACHWORT;
     }
 }
