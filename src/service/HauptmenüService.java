289dbb71dcb04769df25f955832b94f90d400088
Willy <15kha@cloak.mafiasi.de>
2017-05-23 11:00:20 +0200

    neue Klassen

---
+++ src/service/HauptmenüService.java
@@ -0,0 +1,27 @@
+package service;
+
+import material.Kategorie;
+
+public class HauptmenüService
+{
+
+    public void erstelleKategorie(String name)
+    {
+        
+    }
+    
+    public void entferneKategorie(Kategorie kategoriename)
+    {
+        
+    }
+    
+    public void ladeKategorie(Kategorie kategoriename)
+    {
+        
+    }
+    
+    public void bearbeiteKategorie(Kategorie kategorie)
+    {
+        
+    }
+}
